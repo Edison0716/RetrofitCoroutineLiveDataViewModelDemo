827b2da9a681932fcc617f0abb2a7501db8d67da
EdisonLi <18644056528@163.com>
2019-06-06 18:02:50 +0800

    -----协程进一步学习-----

--- app/src/main/java/com/junlong0716/viewmodeldemo/MainActivity.kt
+++ app/src/main/java/com/junlong0716/viewmodeldemo/MainActivity.kt
@@ -4,12 +4,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private var mTimes = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -26,6 +28,15 @@ class MainActivity : AppCompatActivity() {
 
         stockViewModel.mStockLiveData1.observe(this, Observer {
             Toast.makeText(this,it,Toast.LENGTH_SHORT).show()
+        })
+
+        stockViewModel.mTestLiveData.observe(this, Observer {
+            Toast.makeText(this,"执行",Toast.LENGTH_SHORT).show()
+        })
+
+        stockViewModel.mIntervalLikeRxJava.observe(this, Observer {
+            mTimes += 1
+            Toast.makeText(this,"轮询$mTimes",Toast.LENGTH_SHORT).show()
         })
     }
 }
--- app/src/main/java/com/junlong0716/viewmodeldemo/StockViewModel.kt
+++ app/src/main/java/com/junlong0716/viewmodeldemo/StockViewModel.kt
@@ -1,14 +1,11 @@
 package com.junlong0716.viewmodeldemo
 
 import android.util.Log
-import androidx.arch.core.util.Function
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
 import retrofit2.HttpException
 
 /**
@@ -17,22 +14,25 @@ import retrofit2.HttpException
  * Date:     2019/6/6 10:27
  * Description:
  */
-class StockViewModel: ViewModel() {
+class StockViewModel : ViewModel() {
     val mStockLiveData = MutableLiveData<StockBean>()
-    var mStockLiveData1: LiveData<String> =  Transformations.map(mStockLiveData) {
-        it.result[0].data.name
-    }
+    var mStockLiveData1: LiveData<String> = Transformations.map(mStockLiveData) { it.result[0].data.name }
     val mStockRequestErrorMsg = MutableLiveData<String>()
+    val mTestLiveData = MutableLiveData<Any>()
+    val mIntervalLikeRxJava = MutableLiveData<Any>()
 
     init {
-        requestStockLiveInfo()
+        GlobalScope.launch(Dispatchers.IO) {
+            requestStockLiveInfo()
+        }
     }
 
-    private fun requestStockLiveInfo() {
-        GlobalScope.launch(Dispatchers.IO) {
+    private suspend fun requestStockLiveInfo() {
+        val job = GlobalScope.launch(Dispatchers.IO) {
+            delay(5000)
             val requestStockAsync = BaseRetrofitClient.instance.getRetrofitClient().create(Api::class.java)
                 .requestStockAsync("sh601009", "f065fbab3b7e671f6e3cf9b1f8214ee2")
-            Log.d("------请求一次-----","------请求一次-----")
+            Log.d("------请求一次-----", "------请求一次-----")
             try {
                 //子线程发送数据
                 mStockLiveData.postValue(requestStockAsync.await())
@@ -40,6 +40,25 @@ class StockViewModel: ViewModel() {
                 mStockRequestErrorMsg.value = e.message()
             } catch (e: Exception) {
                 mStockRequestErrorMsg.value = e.message
+            }
+        }
+
+        //堵塞线程
+        job.join()
+
+        val job1 = GlobalScope.launch(Dispatchers.Main) {
+            mTestLiveData.value = "OK"
+        }
+
+        //todo 为什么没有挂起？
+        delay(5000)
+
+        job1.join()
+
+        GlobalScope.launch(Dispatchers.IO) {
+            repeat(Int.MAX_VALUE) {
+                mIntervalLikeRxJava.postValue("SEND_EVENT")
+                delay(3000)
             }
         }
     }
